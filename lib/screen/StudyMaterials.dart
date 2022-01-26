288d96bd7d2d7f60f058c42ccc9669fdbd3b7e74
hp <flutter.django@gmail.com>
2022-01-26 21:27:39 +0530

    stdymaterails

---
+++ lib/screen/StudyMaterials.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StudyMaterials extends StatefulWidget {
+  const StudyMaterials({Key? key}) : super(key: key);
+
+  @override
+  _StudyMaterialsState createState() => _StudyMaterialsState();
+}
+
+class _StudyMaterialsState extends State<StudyMaterials> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
